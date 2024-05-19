190ef96dd5011e17c2014c59815828b5e96f71f2
Andrea Spelgatti <spelgattiandrea@gmail.com>
2024-05-19 15:36:31 +0000

    Initial support for the authentification-server

--- mongoDB/init_scripts/init.js
+++ mongoDB/init_scripts/init.js
@@ -1,11 +1,19 @@
-// Function to load JSON data from file and insert into the collection
-function loadAndInsertData(dbName, collectionName, filePath) {
-    const data = JSON.parse(cat(filePath));
-    db = db.getSiblingDB(dbName);
-    db[collectionName].drop(); // Drop the collection if it already exists
-    db.createCollection(collectionName);
-    db[collectionName].insertMany(data);
-}
-
-loadAndInsertData('authentication-srv', 'auth_request', '/docker-entrypoint-initdb.d/auth_request.json');
-loadAndInsertData('authentication-srv', 'user', '/docker-entrypoint-initdb.d/user.json');
+db = db.getSiblingDB('authentication-srv');
+ 
+db.createCollection('auth_request');
+db.createCollection('user');
+db = db.getSiblingDB('user');
+ 
+db.user.insertOne({
+    "registerId": "abcdefghi",
+    "name": "quentin",
+    "lastname": "melis",
+    "birthdate": new Date("1999-11-10T00:00:00Z"),
+    "genre": "H",
+    "phoneNumber": "041234567",
+    "pinCode": 1234,
+    "cardNumber": "1234",
+    "password": "$2y$10$vheS0L6gW8jwtwNG/EgKL.dRKfqumVfwnIkQhoSvqDAU3Vh3s/PDu",
+    "enable": true,
+    "email": "quentin.melis@student.hepl.be"
+});
